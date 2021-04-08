71e73652e33a3a35834162fd42d7fc5dc6cec3dd
axchisolm <axchisolm@gmail.com>
2021-04-08 13:56:00 -0500

    Add function to print board to screen

--- main.c
+++ main.c
@@ -9,13 +9,17 @@
 #include <curses.h>
 #include <unistd.h> // for sleep
 
+#define DELAY 30000
+
 //char** board;
-char board[10][10];
+char board[24][80];
 int max_y, max_x;
 char* board2 = "Hello worldy";
 
 void init();
 void test();
+void set_board();
+void print_screen();
 
 int main(int argc, char* argv[])
 {
@@ -23,27 +27,21 @@ int main(int argc, char* argv[])
 	printf("hello, world\n");
 
 	init();
+	
+	set_board();
 
-	test();
-
-	sleep(1);
-	for (int i=0; i<10; ++i)
-	{
-		for (int j=0; j<10; ++j)
+	for (int i=0; i<max_y;++i)
+		for (int j = 0; j < max_x; j++)
 		{
-			mvprintw(i, j, board[i][j]);
-			refresh();
-			usleep(10000);
+			board[i][j] = 'o';
+			print_screen();
 		}
-		//sleep(1);
-		usleep(10000);
-	}
-
-	mvprintw(0, 0, "hello, world");
-	refresh();
+		
 	sleep(1);
 	
 	endwin();
+
+	printf("max_X: %d\nmax_y: %d\n", max_x, max_y);
 
 	return 0;
 }
@@ -73,4 +71,25 @@ void update()
 void test()
 {
 	mvprintw(0, 0, board2);
+}
+
+void set_board()
+{
+	for (int row=0; row<max_y; ++row)
+	{
+		for (int col=0;col<max_x; ++col)
+		{
+			board[row][col] = 'x';
+		}
+	}
+}
+void print_screen()
+{
+	//clear();
+	for (int row=0; row<max_y; ++row)
+	{
+		mvprintw(row, 0, board[row]);
+	}
+	refresh();
+	usleep(DELAY);
 }
